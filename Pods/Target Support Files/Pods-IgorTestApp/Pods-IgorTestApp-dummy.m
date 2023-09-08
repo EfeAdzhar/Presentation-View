4e4855b412fc5b88a11fda11f9e0a5e5f8e7638c
Efe Adzhar <Efe.Adzhar@nordstrom.com>
2023-09-08 18:05:48 +0200

    Deactivated Podas

--- Pods/Target Support Files/Pods-IgorTestApp/Pods-IgorTestApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IgorTestApp : NSObject
-@end
-@implementation PodsDummy_Pods_IgorTestApp
-@end
--- Pods/Target Support Files/ScrollableStackView/ScrollableStackView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ScrollableStackView : NSObject
-@end
-@implementation PodsDummy_ScrollableStackView
-@end
